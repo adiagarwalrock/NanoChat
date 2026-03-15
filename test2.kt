3de0239980bfc5dac835fb9a5970d4bbe99384f2
adiagarwalrock <aditya.ag1234@gmail.com>
2026-03-14 21:28:55 -0400

    chore: remove temporary reflection scripts for `EngineConfig` and `Conversation` classes.

--- test2.kt
+++
@@ -1,7 +0,0 @@
-import com.google.ai.edge.litertlm.Conversation
-import java.lang.reflect.Modifier
-
-fun main() {
-    val clazz = Conversation::class.java
-    clazz.declaredMethods.forEach { println("${Modifier.toString(it.modifiers)} ${it.returnType.simpleName} ${it.name}(${it.parameterTypes.map { p -> p.simpleName }.joinToString(", ")})") }
-}
